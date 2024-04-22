4165c553bfb641154093089006ca3b29aadd5cf7
uczen <uczen@TE-PIO-D-307M14.local>
2024-04-22 12:21:39 +0200

    first commit

---
+++ app.jsx
@@ -0,0 +1,16 @@
+const Xd = () => {
+    return (
+        <div>
+            <ul>
+                <li>4</li>
+                <li>3</li>
+                <li>2</li>
+                <li>1</li>
+                <li>0</li>
+            </ul>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("mydiv"));
+root.render(<Xd />);
